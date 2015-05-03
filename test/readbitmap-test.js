e10c87432367e8b0f110e3ec8aa0f690f0be3b7b
Satu Bailey <satu.bailey@gmail.com>
2015-05-03 12:37:49 -0700

    add test files

---
+++ test/readbitmap-test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require("chai").expect;
+var readbitmap = require("../lib/readbitmap");
+
+// describe("#getHeaderInfo()", function(){
+//   it("should build a headerInfo object", function(){
+//    expect(headerInfo)to.eql(typeof "Object");
+//   });
+// });
