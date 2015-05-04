5f4c5ee378f36f05023e1563293b8731df99f393
Satu Bailey <satu.bailey@gmail.com>
2015-05-04 12:01:18 -0700

    add tests

--- test/readbitmap-test.js
+++ test/readbitmap-test.js
@@ -1,10 +1,41 @@
 "use strict";
 
+var fs = require("fs");
 var expect = require("chai").expect;
-var readbitmap = require("../lib/readbitmap");
+var readbitmap = require("../lib/readbitmap").getHeaderInfo;
+var transform = require("../lib/transform").transform;
+var header;
 
-// describe("#getHeaderInfo()", function(){
-//   it("should build a headerInfo object", function(){
-//    expect(headerInfo)to.eql(typeof "Object");
-//   });
-// });
+//test that tests will work.
+describe("test testing", function() {
+
+  it("shoud give me a passing test", function(done) {
+    done();
+  });
+
+});
+
+describe("get header info", function() {
+
+  beforeEach(function(done) {
+    fs.readFile("./bitmap1.bmp", function(err, data) {
+      if(err) {
+        return console.log(err);
+      }
+      header = readbitmap(data);
+      done();
+    });
+  });
+
+  it("should show that headerInfo is an object", function() {
+    expect(header).to.be.an("object");
+  });
+
+  it("should get the proper type", function() {
+    expect(header.type).to.eql("BM");
+  });
+
+  it("should get the proper header size", function() {
+    expect(header.size).to.eql(11078);
+  });
+});
--- test/transform-test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-var expect = require("chai").expect;
-var transform = require("../lib/transform");
